88a413e379ebd84fe15dcc9f76b6b89992e6446e
Frol48 <tampuk103@yandex.ru>
2022-03-18 21:46:06 +0300

    add negative tests for product page, methods for login page in base page

--- pages/locators.py
+++ pages/locators.py
@@ -11,6 +11,11 @@ class LoginPageLocators():
 class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
-    ADD_TO_BASKET_BUTTON  = ( By . CSS_SELECTOR , "#add_to_basket_form" )
+    ADD_TO_BASKET_BUTTON  = (By.CSS_SELECTOR , "#add_to_basket_form")
     ADD_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) .alertinner strong")
-    ADD_PRODUCT_PRICE = ( By . CSS_SELECTOR , "#messages .alertinner p strong")
+    ADD_PRODUCT_PRICE = (By.CSS_SELECTOR , "#messages .alertinner p strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR,".alert-success")
+    
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -21,3 +21,11 @@ class ProductPage(BasePage):
         if element.text==text:
             find = True
         return find
+    
+    def should_not_be_success_message(self): 
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+            
+    def success_message_should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+               "Success message does not disappeared, but should be"
